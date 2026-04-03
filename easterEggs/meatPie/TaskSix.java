ace37c5145c730924fe862d4e96cd7db674b56a4
isaac urban <kpomassiurban@gmail.com>
2026-04-04 00:50:12 +0100

    fixed some commit issues

---
+++ easterEggs/meatPie/TaskSix.java
@@ -0,0 +1,11 @@
+public class TaskSix{
+    public static void main(String[] args){
+    
+        int count = 1;
+            
+        System.out.println(count++);
+        System.out.println(count++);
+        System.out.println(count++);
+    }
+}
+
