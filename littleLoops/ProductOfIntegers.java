68fbf0df65d90031f6dd98cd6ce704f2b3e79b76
isaac urban <kpomassiurban@gmail.com>
2026-04-09 02:44:30 +0100

    done with task two

---
+++ littleLoops/ProductOfIntegers.java
@@ -0,0 +1,12 @@
+public class ProductOfIntegers{
+    public static void main(String[] args){
+        int product = 1;
+        int i = 1;
+        while (i <= 10){
+            product = product * i;
+            System.out.println(product);
+            i++;
+        }
+    }
+}
+
