9cd91c598479f507044c8e6cf0f8e8967d16d9ef
isaac urban <kpomassiurban@gmail.com>
2026-04-09 02:07:52 +0100

    done with first task

---
+++ littleLoops/DivisibleByThree.java
@@ -0,0 +1,8 @@
+public class DivisibleByThree{
+    public static void main(String[] args){
+        for (int i = 0; i <= 30; i++){
+            if (i % 3 == 0) System.out.println(i + " is dividible by three");
+        }
+    }
+}
+
