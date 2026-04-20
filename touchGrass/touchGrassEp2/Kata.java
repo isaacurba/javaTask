e6663ba46aa4a83e2fd47beedcbe9c72355236f7
Isaac Urban <kpomassiurban@gmail.com>
2026-04-20 14:45:06 +0100

    Answered the episode in one single file

---
+++ touchGrass/touchGrassEp2/Kata.java
@@ -0,0 +1,135 @@
+public class Kata{
+    public static void main(String... args){
+    
+        System.out.println(divide(10, 21));
+        
+        System.out.println(factorialOf(5));
+        
+        factorOf(10);  
+        
+        System.out.print(isEven(21));
+        
+        System.out.println(isPallindrome(231));
+        
+        System.out.print(isPrimeNumber(20));
+        
+        System.out.print(maximum(20, 5));
+        
+        System.out.println(isPerfectSquare(10));
+        
+        System.out.println(squareOf(50));
+        
+        System.out.print(subtract(10, 20));
+        
+        
+        
+     
+    public static int divide(int firstNumber, int secondNumber){
+        if (firstNumber > secondNumber){
+            return firstNumber / secondNumber;
+        }else{
+            return secondNumber / firstNumber;
+        }
+    }
+    
+    public static int factorialOf(int number){
+        int result = 1;
+        
+        for (int loop = number; loop >= 1; loop--){
+            result *= loop;
+        }
+        
+        return result;
+    }
+    
+    public static void factorOf(int number){
+        for (int startNumber = 1; startNumber <= number; startNumber++){
+            if (number % startNumber == 0){
+                System.out.print(startNumber + " ");
+            }
+        }
+    }
+    
+    public static boolean isEven(int number){
+        if (number % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public static boolean isPallindrome(int number){
+        int originalNumber = number;
+        int lastDigit;
+        int reverseDigit = 0;
+        
+        while (number > 0){
+            lastDigit = number % 10;
+            reverseDigit = (reverseDigit * 10) + lastDigit;
+            number /= 10;
+        }     
+        
+        return originalNumber == reverseDigit;
+    }
+    
+    public static boolean isPrimeNumber(int number){
+        if (number % 2 != 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public static int maximum(int firstNumber, int secondNumber){
+        if (firstNumber > secondNumber){
+            return firstNumber;
+        }else{
+            return secondNumber;
+        }
+    }
+    
+    public static boolean isPerfectSquare(int number){
+        int squareRoot = (int) Math.sqrt(number);
+        
+        if (squareRoot * squareRoot == number){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public static int squareOf(int number){
+        return number * number;
+    }
+    
+    public static int subtract(int firstNumber, int secondNumber){
+        if (firstNumber > secondNumber){
+            return firstNumber - secondNumber;
+        }else{
+            return secondNumber - firstNumber;
+        }
+    }
+   
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- touchGrass/touchGrassEp2/SquareOf.java
+++
@@ -1,9 +0,0 @@
-public class SquareOf{
-    public static void main(String... args){
-        System.out.println(squareOf(50));
-    }
-    
-    public static int squareOf(int number){
-        return number * number;
-    }
-}
