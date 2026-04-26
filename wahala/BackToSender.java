3ae6d8bd9338e330f8e1d3f8bfb3a8f63467e7ed
Isaac Urban <kpomassiurban@gmail.com>
2026-04-26 21:10:44 +0100

    Done with Bitter cola

--- wahala/BackToSender.java
+++ wahala/BackToSender.java
@@ -1,5 +1,20 @@
 public class BackToSender {
+
     public static int allowanceOf(int number){
-        return 45000;
+        int rate;
+        if (number >= 50 && number <= 59){
+           rate = 200;
+        }
+        else if (number >= 60 && number <= 69){
+           rate = 250;
+        }
+        else if (number < 50){
+            rate = 160;
+        }else{
+            rate = 500;
+        }
+        int result = number * rate + 5000;
+        
+        return result;
     }
-}
+}   
--- wahala/BackToSenderTest.java
+++ wahala/BackToSenderTest.java
@@ -5,15 +5,58 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BackToSenderTest{
 
     @Test
-    public void TestThatBackToSenderReturnCorrectValue(){
+    public void TestToCheckIfRateIsLessThan50(){
+        // Given
+        int number = 25;
+        
+        //when
+        int actual = BackToSender.allowanceOf(number);
+        
+        int expected = 9000;
+        
+        assertEquals(actual, expected); 
+    }
+    
+    @Test
+    public void TestToCheckIfRateIsGreaterThan70(){
         // Given
         int number = 80;
         
-        // when
-        int expected = BackToSender.allowanceOf(number);
+        //when
+        int actual = BackToSender.allowanceOf(number);
         
-        // check
-        int actual = 45000;
-        assertEquals(actual, expected);
+        int expected = 45000;
+        
+        assertEquals(actual, expected); 
+    } 
+    
+    @Test
+    public void TestToCheckIfRateIsBetween50and59(){
+        // Given
+        int number = 55;
+        
+        //when
+        int actual = BackToSender.allowanceOf(number);
+        
+        int expected = 16000;
+        
+        assertEquals(actual, expected); 
     }
+    
+    @Test
+    public void TestToCheckIfRateIsBetween60and69(){
+        // Given
+        int number = 65;
+        
+        //when
+        int actual = BackToSender.allowanceOf(number);
+        
+        int expected = 21250;
+        
+        assertEquals(actual, expected); 
+    }
+    
+    
+    
+    
 }
