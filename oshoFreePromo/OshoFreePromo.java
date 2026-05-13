b638cbf63e7892270bc79a99cba07e78c6e004f3
Isaac Urban <kpomassiurban@gmail.com>
2026-05-13 13:01:08 +0100

    updated the ignore case logical concept

--- oshoFreePromo/OshoFreePromo.java
+++ oshoFreePromo/OshoFreePromo.java
@@ -13,19 +13,19 @@ public class OshoFreePromo {
         
         int total = cartTotal;
         
-        if (promoCode == null && promoCode.isEmpty()){
+        if (promoCode == null || promoCode.isEmpty()){
             total = cartTotal;
         }
         if (cartTotal < 5000){
             total = cartTotal;
         }
-        if (cartTotal >= 5000 && cartTotal <= 14999 && promoCode.toLowerCase().equalsIgnoreCase("STARTER10")){
+        if (cartTotal >= 5000 && cartTotal <= 14999 && promoCode.equalsIgnoreCase("STARTER10")){
             total = cartTotal - (cartTotal * 10 / 100);
         } 
-        if (cartTotal >= 15000 && cartTotal <= 29999 && promoCode.toLowerCase().equalsIgnoreCase("BIGBOY20")){
+        if (cartTotal >= 15000 && cartTotal <= 29999 && promoCode.equalsIgnoreCase("BIGBOY20")){
             total = cartTotal - (cartTotal * 20 / 100);
-        } 
-        if (cartTotal >= 30000 && promoCode.toLowerCase().equalsIgnoreCase("OSHOFREE35")){
+        }       
+        if (cartTotal >= 30000 && promoCode.equalsIgnoreCase("OSHOFREE35")){
             total = cartTotal - (cartTotal * 35 / 100);
         } 
         
